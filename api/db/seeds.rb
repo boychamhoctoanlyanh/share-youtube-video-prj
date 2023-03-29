40400fc41b77fbc452c89f1fbce70e6bf2e15436
Pham Minh <pham.minh301@gmail.com>
2023-03-29 19:14:00 +0900

    Add test user info to README

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -4,13 +4,13 @@ user3 = User.create(username: "user3", password: "password")
 user4 = User.create(username: "user4", password: "password")
 user5 = User.create(username: "user5", password: "password")
 
-user1_movie1 = Movie.create(url: "https://www.youtube.com/watch?v=1", user: user1)
-user1_movie2 = Movie.create(url: "https://www.youtube.com/watch?v=2", user: user1)
+user1_movie1 = Movie.create(url: "https://www.youtube.com/watch?v=O8MnTgASxJ4", user: user1)
+user1_movie2 = Movie.create(url: "https://www.youtube.com/watch?v=-7sISWuTdj0", user: user1)
 
-user2_movie1 = Movie.create(url: "https://www.youtube.com/watch?v=3", user: user2)
-user2_movie2 = Movie.create(url: "https://www.youtube.com/watch?v=4", user: user2)
+user2_movie1 = Movie.create(url: "https://www.youtube.com/watch?v=6kq-RRK4Q8w", user: user2)
+user2_movie2 = Movie.create(url: "https://www.youtube.com/watch?v=tcat9CPiAZ4", user: user2)
 
-user3_movie1 = Movie.create(url: "https://www.youtube.com/watch?v=5", user: user3)
+user3_movie1 = Movie.create(url: "https://www.youtube.com/watch?v=wEBgnZl9L0o", user: user3)
 
 vote_types = Vote.vote_types.keys
 
